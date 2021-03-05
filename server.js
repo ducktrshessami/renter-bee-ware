7cbf6131369b0476e078f37e35360696ef5b9c32
ducktrshessami <jwee761@gmail.com>
2021-03-04 20:57:31 -0800

    sync models before starting api server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const db = require("./models");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,10 +15,14 @@ require("./routes/api")(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+db.sequelize.sync()
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+    });
+  })
+  .catch(console.error);
