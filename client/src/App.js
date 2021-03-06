2eaf92a0c54e6ffc7caf24721c949dddc9bb9b0f
NDPenaloza <ndpenaloza@gmail.com>
2021-03-06 00:11:59 -0800

    imported pages to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Index from './pages/Index';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import SearchApt from './pages/SearchApt';
+import Member from './pages/Member';
+import WriteReview from './pages/WriteReview';
+import EditReview from './pages/EditReview';
+import Results from './pages/Results';
+
+
 
 class App extends Component {
   render() {
