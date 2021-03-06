4556a3f3a69dab712ee52d06cb6711fbe46cbd3b
ducktrshessami <jwee761@gmail.com>
2021-03-06 12:46:54 -0800

    Places searching functions

---
+++ utils/places.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+
+module.exports = function (apikey) {
+    function findPlaceFromText(query) {
+        return fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apikey}&input=${query}&inputtype=textquery&fields=place_id,formatted_address,name,types`)
+            .then(res => res.json());
+    }
+
+    function nearbySearch({latitude, longitude}, radius, type) {
+        let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apikey}&location=${latitude},${longitude}&radius=${radius}`;
+        if (type) {
+            url += `&type=${type}`;
+        }
+        return fetch(url)
+            .then(res => res.json());
+    }
+
+    function textSearch(options) {
+        let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${apikey}`;
+        for (let param in options) {
+            url += `&${param}=${options[param]}`;
+        }
+        return fetch(url)
+            .then(res => res.json());
+    }
+
+    return {
+        findPlaceFromText,
+        nearbySearch,
+        textSearch
+    };
+};
