dabf349f796cc92ae7e5887ed26e4e20437f449c
ducktrshessami <jwee761@gmail.com>
2021-03-06 12:53:31 -0800

    fixed Places wrapper for use in javascript

--- client/src/utils/Places.js
+++ client/src/utils/Places.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-module.exports = function (apikey) {
+export default function (apikey) {
     function findPlaceFromText(query) {
         return fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apikey}&input=${query}&inputtype=textquery&fields=place_id,formatted_address,name,types`)
             .then(res => res.json());
