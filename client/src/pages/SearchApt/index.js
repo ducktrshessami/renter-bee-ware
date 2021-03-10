06146ed00a971fca92d794d735792c3fed821deb
Emily Thomas <ethomas@quint.co.uk>
2021-03-09 21:33:14 -0800

    returned html for search page

--- client/src/pages/SearchApt/index.js
+++ client/src/pages/SearchApt/index.js
@@ -1,5 +1,11 @@
-function SearchApt() {
+import React from 'react';
 
+function SearchApt() {
+    return (
+        <div>
+            <p>Search-Apartment-Here</p>
+        </div>
+    )
 }
 
 export default SearchApt;
