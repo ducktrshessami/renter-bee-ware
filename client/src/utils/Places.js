5103579d40b17d92594149a640c0e12f0a4d1df8
ducktrshessami <jwee761@gmail.com>
2021-03-11 18:56:39 -0800

    converted Places back to javascript for use in react

--- client/src/utils/Places.js
+++ client/src/utils/Places.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-module.exports = function (apikey) {
+export default function Places(apikey) {
     function findPlaceFromText(query) {
         return fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apikey}&input=${query}&inputtype=textquery&fields=place_id,formatted_address,name,types,geometry`)
             .then(res => res.json());
