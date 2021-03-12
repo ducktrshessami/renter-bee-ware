0ed78e5b263df92576579acf8dabd323a44fb134
NDPenaloza <ndpenaloza@gmail.com>
2021-03-11 20:50:14 -0800

    added footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,4 +1,30 @@
 function Footer() {
+  return(
+    <footer className="page-footer">
+    <div className="container">
+      <div className="row">
+        <div className="col l6 s12">
+          <h5 className="white-text">Renter Bee-Ware</h5>
+          <a className="white-text" href="https://github.com/ducktrshessami/renter-bee-ware" target="_blank" rel="noreferrer">GitHub Repository</a>
+        </div>
+        <div className="col l4 offset-l2 s12">
+          <h5 className="white-text">Team Members</h5>
+          <ul>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/ducktrshessami" target="_blank" rel="noreferrer">Josh Wee</a></li>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/ethomas22" target="_blank" rel="noreferrer">Emily Thomas</a></li>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/dillonhoban" target="_blank" rel="noreferrer">Dillon Hoban</a></li>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/ndpenaloza" target="_blank" rel="noreferrer">Nick D. Penaloza</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="footer-copyright">
+      <div className="container center-align">
+      Renter Bee-Ware © 2021
+      </div>
+    </div>
+  </footer>
+  )
 
 }
 
