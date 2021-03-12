1786cfed2d86868b1e145eb8b538f4c4dd95bf19
ducktrshessami <jwee761@gmail.com>
2021-03-11 20:36:19 -0800

    post review route

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,12 @@
-module.exports = function(app) {
+const db = require("../models");
 
+module.exports = function (app) {
+    app.post("/api/review", function (req, res) {
+        db.Review.create(req.body)
+            .then(data => res.status(200).json(data))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
+    });
 };
