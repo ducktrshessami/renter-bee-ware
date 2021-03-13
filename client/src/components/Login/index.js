418b7978ef7969979355de2c6d84d74db76cb8b2
NDPenaloza <ndpenaloza@gmail.com>
2021-03-13 10:41:22 -0800

    added signup component

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,9 +3,38 @@ import { Component } from 'react';
 class Login extends Component {
 render() {
   return(
-    <div>
-
-    </div>
+      <div className="container">
+        <div className="row">
+          <form className="col s12">
+            <h4 className="col s12 center-align">Sign up</h4>
+            <div className="row">
+              <div className="input-field col s8 offset-s2">
+                <input id="first_name" type="text" className="validate"/>
+                <label htmlFor="first_name">First Name</label>
+              </div>
+            </div>
+            <div className='row'>
+              <div className="input-field col s8 offset-s2">
+                <input id="last_name" type="text" className="validate"/>
+                <label htmlFor="last_name">Last Name</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s8 offset-s2">
+                <input id="password" type="password" className="validate"/>
+                <label htmlFor="password">Email</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s8 offset-s2">
+                <input id="password" type="password" className="validate"/>
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <button className="btn waves-effect waves-light col offset-s2" type="submit" name="action">Sign up</button>
+          </form>
+        </div>
+      </div>
   )
 }
 
