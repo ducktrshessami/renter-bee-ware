648f6b8b603daaea022498314ec81a97edec351e
ducktrshessami <jwee761@gmail.com>
2021-03-13 11:35:34 -0800

    API util handles place api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,6 +8,26 @@ const API = {
             headers: { 'Content-Type': 'application/json' }
         })
             .then(res => res.json());
+    },
+    findPlaceFromText: function (query) {
+        return fetch(`/api/findplacefromtext?q=${query}`)
+            .then(res => res.json());
+    },
+    nearbySearch: function ({ latitude, longitude }, radius, type) {
+        let url = `/api/nearbysearch?radius=${radius}&latitude=${latitude}&longitude=${longitude}`;
+        if (type) {
+            url += "&type=" + type;
+        }
+        return fetch(url)
+            .then(res => res.json());
+    },
+    placeDetails: function (place_id, sessiontoken) {
+        let url = "/api/details/" + place_id;
+        if (sessiontoken) {
+            url += "?sessiontoken=" + sessiontoken;
+        }
+        return fetch(url)
+            .then(res => res.json());
     }
 };
 
