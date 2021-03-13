40ffbef785b7d6cbbd3b596b027a40a09a6ad5c9
ducktrshessami <jwee761@gmail.com>
2021-03-13 11:58:34 -0800

    use json for server requests

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const db = require("./models");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
