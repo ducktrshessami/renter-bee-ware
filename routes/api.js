d931ec40e07872febeb925a463d41f3f6c26d064
ducktrshessami <jwee761@gmail.com>
2021-03-13 12:52:10 -0800

    newReview route

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,25 @@ const db = require("../models");
 
 module.exports = function (app) {
     app.post("/api/review", function (req, res) {
-        db.Review.create(req.body)
+        db.Place.findOne({ where: { place_id: req.body.place_id } })
+            .then(async place => {
+                let PlaceId;
+                if (place) {
+                    PlaceId = place.id;
+                }
+                else {
+                    PlaceId = (await db.Place.create({
+                        place_id: req.body.place_id,
+                        formatted_address: req.body.formatted_address,
+                        name: req.body.name,
+                        types: req.body.types,
+                        latitude: req.body.geometry.location.lat,
+                        longitude: req.body.geometry.location.lng
+                    })).id;
+                }
+                return PlaceId;
+            })
+            .then(PlaceId => db.Review.create({ ...req.body, PlaceId: PlaceId }))
             .then(data => res.status(200).json(data))
             .catch(err => {
                 console.error(err);
