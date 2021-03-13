72e77448bce51a8265e6c33019b50c510da2347a
Emily Thomas <ethomas@quint.co.uk>
2021-03-13 13:29:09 -0800

    added imports

--- client/src/pages/SearchApt/index.js
+++ client/src/pages/SearchApt/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import API from "client/src/utils/API";
+
+
+
+
 
 function SearchApt() {
   return (
@@ -9,31 +14,31 @@ function SearchApt() {
           <div className="row">
             <div className="input-field col s6">
               <input id="street-address" type="text" />
-              <label htmlFor="street-address">Street Address</label>
+              <label for="street-address">Street Address</label>
             </div>
             <div class="input-field col s6">
               <input id="apt-number" type="text" />
-              <label htmlFor="apt-number">Apt Number</label>
+              <label for="apt-number">Apt Number</label>
             </div>
           </div>
           <div className="row">
             <div class="input-field col s6">
               <input id="city" type="text" />
-              <label htmlFor="city">City</label>
+              <label for="city">City</label>
             </div>
             <div className="input-field col s6">
               <input id="state" type="text" />
-              <label htmlFor="state">State</label>
+              <label for="state">State</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s6">
               <input id="zip-code" type="text" />
-              <label htmlFor="zip-code">Zip Code</label>
+              <label for="zip-code">Zip Code</label>
             </div>
           </div>
           <button
-            className="btn waves-effect btn-large waves-light"
+            class="btn waves-effect waves-light"
             type="submit"
             name="action"
           >
