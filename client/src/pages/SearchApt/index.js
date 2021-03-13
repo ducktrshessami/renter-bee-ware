6ddfed9ec372bbdf9723cd510fa594469e0c8861
Emily Thomas <ethomas@quint.co.uk>
2021-03-13 13:15:02 -0800

    fixed submit button

--- client/src/pages/SearchApt/index.js
+++ client/src/pages/SearchApt/index.js
@@ -9,31 +9,31 @@ function SearchApt() {
           <div className="row">
             <div className="input-field col s6">
               <input id="street-address" type="text" />
-              <label for="street-address">Street Address</label>
+              <label htmlFor="street-address">Street Address</label>
             </div>
             <div class="input-field col s6">
               <input id="apt-number" type="text" />
-              <label for="apt-number">Apt Number</label>
+              <label htmlFor="apt-number">Apt Number</label>
             </div>
           </div>
           <div className="row">
             <div class="input-field col s6">
               <input id="city" type="text" />
-              <label for="city">City</label>
+              <label htmlFor="city">City</label>
             </div>
             <div className="input-field col s6">
               <input id="state" type="text" />
-              <label for="state">State</label>
+              <label htmlFor="state">State</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s6">
               <input id="zip-code" type="text" />
-              <label for="zip-code">Zip Code</label>
+              <label htmlFor="zip-code">Zip Code</label>
             </div>
           </div>
           <button
-            class="btn waves-effect waves-light"
+            className="btn waves-effect btn-large waves-light"
             type="submit"
             name="action"
           >
