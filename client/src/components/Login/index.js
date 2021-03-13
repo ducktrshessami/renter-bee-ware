d9d1a4c3dabee68220d0d869574ef782a5d4fa34
NDPenaloza <ndpenaloza@gmail.com>
2021-03-13 11:04:20 -0800

    added login component

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,40 +1,28 @@
-import { Component } from 'react';
+import React from 'react';
 
-class Login extends Component {
-render() {
+function Login() {
+
   return(
-      <div className="container">
+    <div className='container'>
+    <div className="row">
+      <form className="col s12">
+        <h5 className="col s12 center-align">Log In</h5>
         <div className="row">
-          <form className="col s12">
-            <h4 className="col s12 center-align">Sign Up</h4>
-            <div className="row">
-              <div className="input-field col s8 offset-s2">
-                <input id="first_name" type="text" className="validate"/>
-                <label htmlFor="first_name">First Name</label>
-              </div>
-            </div>
-            <div className='row'>
-              <div className="input-field col s8 offset-s2">
-                <input id="last_name" type="text" className="validate"/>
-                <label htmlFor="last_name">Last Name</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s8 offset-s2">
-                <input id="password" type="password" className="validate"/>
-                <label htmlFor="password">Email</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s8 offset-s2">
-                <input id="password" type="password" className="validate"/>
-                <label htmlFor="password">Password</label>
-              </div>
-            </div>
-            <button className="btn waves-effect waves-light col offset-s2" type="submit" name="action">Sign up</button>
-          </form>
+          <div className="input-field col s8 offset-s2">
+            <input id="password" type="password" className="validate"/>
+            <label htmlFor="password">Email</label>
+          </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="input-field col s8 offset-s2">
+            <input id="password" type="password" className="validate"/>
+            <label htmlFor="password">Password</label>
+          </div>
+        </div>
+        <button className="btn waves-effect waves-light col offset-s2" type="submit" name="action">Log In</button>
+      </form>
+    </div>
+  </div>
   )
 }
 
