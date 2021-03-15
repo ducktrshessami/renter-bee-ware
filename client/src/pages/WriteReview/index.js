8d33d82b9ec36ad89751f2e9a5f508b193ba0893
NDPenaloza <ndpenaloza@gmail.com>
2021-03-15 00:51:11 -0700

    made NewReview a self closing take in write review page

--- client/src/pages/WriteReview/index.js
+++ client/src/pages/WriteReview/index.js
@@ -3,9 +3,7 @@ import NewReview from "../../components/NewReview"
 
 function WriteReview() {
     return(
-            <NewReview>
-
-            </NewReview>
+            <NewReview/>
     );
 }
 
