9b6fa6775101e20c2ac8b925721df6c87f9c7539
NDPenaloza <ndpenaloza@gmail.com>
2021-03-15 00:21:57 -0700

    changed class to class name in members page

--- client/src/pages/Member/index.js
+++ client/src/pages/Member/index.js
@@ -2,7 +2,35 @@ import React from "react";
 
 function Member() {
   return (
-
+    <div className='container'>
+    <div className='card'>
+      <h3 className="left-align">Welcome, Stranger</h3>
+    </div>
+    <div className='row'>
+      <div className='row'>
+        <div className="col">
+          <h5 className="center-align">Search Apartments, Write, Edit, and Delete Reviews Here.</h5>
+        </div>
+      </div>
+    <div className="col l3">
+      <div className="collection z-depth-2">
+        <a href="/search-apt" className="collection-item center-align">Search Apartment</a>
+        <a href="/write-review" className="collection-item center-align">Write Review</a>
+      </div>
+    </div>
+    <div className="col s12 l9">
+      <div className="card z-depth-2">
+        <div className="card-content">
+          <span className="card-title">123 Fake Street Springfield </span>
+          <p>Dates Occupied: 1/1/2000- 1/1/2020</p>
+          <p>Review: Real shithole</p>
+          <button className="btn-large waves-effect">Edit Review</button>
+          <button className="btn-large waves-effect">Delete Review</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
   )
 
 }
