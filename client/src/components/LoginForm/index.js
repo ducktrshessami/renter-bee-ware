ee381cab57780faa8e75925f6365108de7e916b7
NDPenaloza <ndpenaloza@gmail.com>
2021-03-17 00:59:02 -0700

    changed indenting of login form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -8,33 +8,33 @@ function LoginForm() {
       initial={{ y: -500}}
       animate={{ y: 0}}
       transition={{ duration: .5}}
-    className='container'>
-    <div className="row">
-      <form className="col s12">
-        <div className="card z-depth-2">
-        <h5 className="col s12 center-align">Log In</h5>
-        <div className="row">
-          <div className="input-field col s8 offset-s2">
-            <input id="password" type="password" className="validate"/>
-            <label htmlFor="password">Email</label>
+      className='container'>
+      <div className="row">
+        <form className="col s12">
+          <div className="card z-depth-2">
+          <h5 className="col s12 center-align">Log In</h5>
+          <div className="row">
+            <div className="input-field col s8 offset-s2">
+              <input id="password" type="password" className="validate"/>
+              <label htmlFor="password">Email</label>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s8 offset-s2">
-            <input id="password" type="password" className="validate"/>
-            <label htmlFor="password">Password</label>
+          <div className="row">
+            <div className="input-field col s8 offset-s2">
+              <input id="password" type="password" className="validate"/>
+              <label htmlFor="password">Password</label>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <button className="btn waves-effect waves-light col offset-s2 btn-large" type="submit" name="action">Log In</button>
-        </div>
-        <div className="row">
-          <a href="/sign-up">Sign Up</a>
-        </div>
-        </div>
-      </form>
-    </div>
-  </motion.div>
+          <div className="row">
+            <button className="btn waves-effect waves-light col offset-s2 btn-large" type="submit" name="action">Log In</button>
+          </div>
+          <div className="row">
+            <a href="/sign-up">Sign Up</a>
+          </div>
+          </div>
+        </form>
+      </div>
+    </motion.div>
   )
 }
 
