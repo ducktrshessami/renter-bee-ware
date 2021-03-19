e1265099fb55606a33949b9d25b29c04e6d23de4
ducktrshessami <jwee761@gmail.com>
2021-03-18 19:35:33 -0700

    handle props for ResultsCard

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
+const placeholderImage = "https://via.placeholder.com/100";
+
 export default class ResultsCard extends Component {
   componentDidMount() {
-
+    // fetch("/api/")
   }
 
   render() {
@@ -13,14 +15,12 @@ export default class ResultsCard extends Component {
             <h3 className="header">Search Results</h3>
             <div className="card horizontal light-blue lighten-4">
               <div className="card-image">
-                <img />
+                <img src={this.props.photo || placeholderImage} alt={this.props.name} />
               </div>
               <div className="card-stacked">
                 <div className="card-content">
-                  <p className={props.name}>
-                    emily
-                </p>
-                  <p className={props.address}>123 Main</p>
+                  <p>{this.props.name}</p>
+                  <p>{this.props.address}</p>
                 </div>
                 <div className="card-action">
                   <a href=""></a>
