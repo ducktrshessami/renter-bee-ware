c88264024d23634cf993382b2fe045e4995bb7f1
ducktrshessami <jwee761@gmail.com>
2021-03-18 19:15:55 -0700

    convert ResultsCard to Component class

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 
+export default class ResultsCard extends Component {
+  componentDidMount() {
 
+  }
 
-export default function ResultsCard(props) {
-  return (
-    <div className="container">
-      <div className="row valign-wrapper">
-        <div className="col s7 offset-s3 valign">
-          <h3 className="header">Search Results</h3>
-          <div className="card horizontal light-blue lighten-4">
-            <div className="card-image">
-              <img/>
-            </div>
-            <div className="card-stacked">
-              <div className="card-content">
-                <p className={props.name}>
-                  emily
-                </p>
-                <p className={props.address}>123 Main</p>
+  render() {
+    return (
+      <div className="container" >
+        <div className="row valign-wrapper">
+          <div className="col s7 offset-s3 valign">
+            <h3 className="header">Search Results</h3>
+            <div className="card horizontal light-blue lighten-4">
+              <div className="card-image">
+                <img />
               </div>
-              <div className="card-action">
-                <a href=""></a>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p className={props.name}>
+                    emily
+                </p>
+                  <p className={props.address}>123 Main</p>
+                </div>
+                <div className="card-action">
+                  <a href=""></a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
