594560ce68a19b519578461cf61faec5f2b245fb
ducktrshessami <jwee761@gmail.com>
2021-03-18 21:39:40 -0700

    allow starting rating and static rating

--- client/src/components/StarRating/index.js
+++ client/src/components/StarRating/index.js
@@ -5,26 +5,27 @@ import StarRatings from 'react-star-ratings';
 
 
 class StarRating extends Component {
+  state = { rating: this.props.rating || 0 };
+
   changeRating(newRating, name) {
     this.setState({
       rating: newRating
     });
   }
 
-  state={rating: 0};
   render() {
     // rating = 0;
-    return ( <
-      StarRatings rating = {
+    return (<
+      StarRatings rating={
         this.state.rating
       }
-      starRatedColor = "#F2AF34"
-      changeRating = {rating => this.changeRating(rating)}
-      numberOfStars = {5}
-      starHoverColor  = "#C1EDFE"
-      starDimension = "2rem"
-      name = 'rating' /
-      >
+      starRatedColor="#F2AF34"
+      changeRating={this.props.static ? undefined : rating => this.changeRating(rating)}
+      numberOfStars={5}
+      starHoverColor="#C1EDFE"
+      starDimension="2rem"
+      name='rating' /
+    >
     );
   }
 }
