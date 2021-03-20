e5a793a643830d9b718827ed2d34350c4f76fb4a
ducktrshessami <jwee761@gmail.com>
2021-03-20 13:39:28 -0700

    allow titleProp prop to dictate review title

--- client/src/components/ReviewResults/index.js
+++ client/src/components/ReviewResults/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReviewCard from "./ReviewCard";
 
-function ReviewResults({ reviews }) {
+function ReviewResults({ reviews, titleProp }) {
   return (
     <ul className="row">
-      {reviews.map(review => <ReviewCard key={review.id} {...review} />)}
+      {reviews.map(review => <ReviewCard key={review.id} title={review[titleProp]} {...review} />)}
     </ul>
   )
 }
