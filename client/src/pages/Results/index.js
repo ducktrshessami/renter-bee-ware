3349b29e36417e3d0324132c01db53df573f6092
ducktrshessami <jwee761@gmail.com>
2021-03-20 13:07:38 -0700

    handle result place db querying

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,23 +1,59 @@
 import React, { Component } from 'react';
 import PlaceInfo from "../../components/PlaceInfo";
 import ReviewResults from '../../components/ReviewResults';
+import API from "../../utils/API";
 
 class Results extends Component {
   state = {
-    placeId: ""
+    place_id: "",
+    reviews: [],
+    address: "",
+    name: "",
+    url: "",
+    photo: "",
+    latitude: null,
+    longitude: null
   }
 
   componentDidMount() {
     let query = new URLSearchParams(window.location.search);
-    this.setState({ placeId: query.get("place") || "" });
+    let place_id = query.get("place");
+    if (place_id) {
+      this.setState({
+        ...this.state,
+        place_id: place_id
+      });
+      API.getReviewsFromPlaceId(place_id)
+        .then(res => {
+          if (Object.keys(res).length) {
+            this.setState({
+              ...this.state,
+              address: res.formatted_address,
+              name: res.name,
+              url: res.url,
+              photo: res.photo,
+              reviews: res.Reviews,
+              latitude: res.latitude,
+              longitude: res.longitude
+            });
+          }
+          else {
+            this.handleNewPlace(place_id);
+          }
+        });
+    }
+  }
+
+  handleNewPlace() {
+    //do this
   }
 
   render() {
     return (
       <div>
-        <PlaceInfo placeId={this.state.place_id} />
+        <PlaceInfo {...this.state} />
         <br />
-        <ReviewResults placeId={this.state.place_id} />
+        <ReviewResults reviews={this.state.reviews} />
       </div>
     );
   }
