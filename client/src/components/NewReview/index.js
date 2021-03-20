51822f7af53b43102ada70fc74797f89e8b10ef4
ducktrshessami <jwee761@gmail.com>
2021-03-20 12:28:21 -0700

    review posting fixes for latest features

--- client/src/components/NewReview/index.js
+++ client/src/components/NewReview/index.js
@@ -5,31 +5,7 @@ import StarRating from "../StarRating";
 import StateSelector from "../StateSelector";
 
 function validate({ streetAddress, aptNumber, city, state, zipCode, startDate, endDate, stars }) {
-  return Boolean(streetAddress.match(/^[0-9]+ .+/i) && (aptNumber ? !Number.isNan(Number(aptNumber)) : true) && city && state && !Number.isNan(Number(zipCode)) && startDate && endDate && !Number.isNaN(Number(stars)));
-}
-
-function submit(event) {
-  event.preventDefault();
-  let reviewData = {
-    streetAddress: event.target["street-address"].value.trim(),
-    aptNumber: event.target["apt-number"].value.trim(),
-    city: event.target["city"].value.trim(),
-    state: event.target["state"].value.trim(),
-    zipCode: event.target["zip-code"].value.trim(),
-    startDate: event.target["start-date"].value.trim(),
-    endDate: event.target["end-date"].value.trim(),
-    stars: this.state.stars,
-    message: event.target["review"].value.trim()
-  };
-  if (validate(reviewData)) {
-    API.findPlaceFromText(`${reviewData.streetAddress}, ${reviewData.city}, ${reviewData.state} ${reviewData.zipCode}`)
-      .then(res => res.candidates[0])
-      .then(place => API.newReview({ ...reviewData, ...place }))
-      .then(() => {
-        window.location.pathname = "/";
-      })
-      .catch(console.error);
-  }
+  return Boolean(streetAddress.match(/^[0-9]+ .+/i) && (aptNumber ? !Number.isNaN(Number(aptNumber)) : true) && city && state && !Number.isNaN(Number(zipCode)) && startDate && endDate && !Number.isNaN(Number(stars)));
 }
 
 class NewReview extends Component {
@@ -42,7 +18,6 @@ class NewReview extends Component {
     M.FormSelect.init(stateSelector);
     this.handleQuery();
   };
-
 
   handleQuery() {
     let query = new URLSearchParams(window.location.search);
@@ -64,12 +39,36 @@ class NewReview extends Component {
     this.setState({ stars: rating });
   }
 
+  submit(event) {
+    event.preventDefault();
+    let reviewData = {
+      streetAddress: event.target["street-address"].value.trim(),
+      aptNumber: event.target["apt-number"].value.trim(),
+      city: event.target["city"].value.trim(),
+      state: event.target["state"].value.trim(),
+      zipCode: event.target["zip-code"].value.trim(),
+      startDate: event.target["start-date"].value.trim(),
+      endDate: event.target["end-date"].value.trim(),
+      stars: this.state.stars,
+      message: event.target["review"].value.trim()
+    };
+    if (validate(reviewData)) {
+      API.findPlaceFromText(`${reviewData.streetAddress}, ${reviewData.city}, ${reviewData.state} ${reviewData.zipCode}`)
+        .then(res => res.candidates[0])
+        .then(place => API.newReview({ ...reviewData, ...place }))
+        .then(() => {
+          window.location.pathname = "/";
+        })
+        .catch(console.error);
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="row">
-            <form className="col s12" onSubmit={submit}>
+            <form className="col s12" onSubmit={event => this.submit(event)}>
               <div className="row">
                 <div className="input-field col s6">
                   <input id="street-address" type="text" className="validate" />
@@ -86,7 +85,7 @@ class NewReview extends Component {
                   <label htmlFor="city">City</label>
                 </div>
                 <div className="input-field col s3">
-                  <StateSelector />
+                  <StateSelector id="state" />
                 </div>
                 <div className="input-field col s3">
                   <input id="zip-code" type="text" className="validate" />
