262cbea6023bbcfbac8c3f3872784d8441d707b6
ducktrshessami <jwee761@gmail.com>
2021-03-20 13:48:14 -0700

    handle titleProp

--- client/src/components/ReviewResults/index.js
+++ client/src/components/ReviewResults/index.js
@@ -4,7 +4,7 @@ import ReviewCard from "./ReviewCard";
 function ReviewResults({ reviews, titleProp }) {
   return (
     <ul className="row">
-      {reviews.map(review => <ReviewCard key={review.id} title={review[titleProp]} {...review} />)}
+      {reviews.map(review => <ReviewCard key={review.id} title={titleProp.toLowerCase().trim() === "user" ? review.User.name : review.Place.formatted_address} {...review} />)}
     </ul>
   )
 }
