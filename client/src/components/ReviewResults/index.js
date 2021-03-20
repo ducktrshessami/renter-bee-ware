721baf76500607008e4d50f89f89ddea9005918c
ducktrshessami <jwee761@gmail.com>
2021-03-20 13:22:54 -0700

    pass each review to ReviewCard subcomponent

--- client/src/components/ReviewResults/index.js
+++ client/src/components/ReviewResults/index.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
+import ReviewCard from "./ReviewCard";
 
 class ReviewResults extends Component {
   render() {
-    return(
-      <div className="row">
-        <div className="col s12 m6">
-          <div className="card">
-            <div className="card-content">
-              <span className="card-title">Address</span>
-              <p>Dates Occupied:</p>
-              <p>Stars:</p>
-              <p>Review:</p>
-            </div>
-          </div>
-        </div>
-      </div>
+    return (
+      <ul className="row">
+        {this.props.reviews.map(review => <ReviewCard key={review.id} {...review} />)}
+      </ul>
     )
   }
 }
