e03ca6ce490a68ca1e2be3938cb2ef1b7da3e46f
Emily Thomas <ethomas@quint.co.uk>
2021-03-21 18:46:10 -0700

    added app.put function

--- client/src/pages/EditReview/index.js
+++ client/src/pages/EditReview/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import EditReviewForm from "../../components/EditReviewForm";
 
 function EditReview() {
-  return(
-    <EditReviewForm/>
-  )
+  return (
+    <div className="container">
+      <h2 className="center-align">Edit Review</h2>
+      <EditReviewForm />
+    </div>
+  );
 }
 
 export default EditReview;
