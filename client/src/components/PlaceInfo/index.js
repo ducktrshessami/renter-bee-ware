9a7929c1472843f7732a776f1ce3cfcbf70413aa
ducktrshessami <jwee761@gmail.com>
2021-03-21 18:05:04 -0700

    display PlaceInfo

--- client/src/components/PlaceInfo/index.js
+++ client/src/components/PlaceInfo/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-export default function PlaceInfo() {
+const placeholderImage = "https://via.placeholder.com/100";
+
+export default function PlaceInfo({ address, name, photo, url }) {
     return (
-        <div></div>
+        <section className="row">
+            <div className="col s12 m8 offset-m2">
+                <div className="card horizontal">
+                    <div className="card-image">
+                        <img src={photo || placeholderImage} alt={name} />
+                    </div>
+                    <div className="card-stacked">
+                        <div className="card-content">
+                            <h3>{name}</h3>
+                            <p>{address}</p>
+                        </div>
+                        <div className="card-action">
+                            {url ? <a href={url}>Website</a> : undefined}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
