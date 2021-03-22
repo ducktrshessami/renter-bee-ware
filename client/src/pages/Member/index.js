60ecc32088691e2f3f63e6fcf1b32585b1cb573e
Dillon H <dhobs21@gmail.com>
2021-03-21 18:18:15 -0700

    styling

--- client/src/pages/Member/index.js
+++ client/src/pages/Member/index.js
@@ -3,41 +3,49 @@ import { motion } from 'framer-motion';
 
 function Member() {
   return (
-    <div className="center-align">
-    <div className='hero-image center-align valign-center'>
-      <motion.h1 
+  <div>
+    <div className="hero-image">
+        <motion.h2 
         initial={{opacity: 0}}
         animate={{opacity: 1}}
-        transition={{duration: 4}}
-        className="center-align" style={{color: "white"}}>Welcome, Stranger</motion.h1>
+          transition={{duration: 4}}
+          className="hero-text">Welcome, Stranger</motion.h2>
+          <motion.h5 
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: 4}}
+          className="hero-text">Search Apartments, Write, Edit, and Delete Reviews Here</motion.h5>
+          
     </div>
     <div className="container">
-    <div className='row'>
       <div className='row'>
-        <div className="col">
-          <h5 className="center-align">Search Apartments, Write, Edit, and Delete Reviews Here.</h5>
+        <div className='row'>
+          <div className="col">
+          </div>
+        </div>
+      <div className="col l3">
+        <div className="collection z-depth-2">
+          <a href="/search-apt" className="collection-item center-align">Search Apartment</a>
+          <a href="/write-review" className="collection-item center-align">Write Review</a>
         </div>
       </div>
-    <div className="col l3">
-      <div className="collection z-depth-2">
-        <a href="/search-apt" className="collection-item center-align">Search Apartment</a>
-        <a href="/write-review" className="collection-item center-align">Write Review</a>
+      <div>
+        <h5>My Reviews</h5>
       </div>
-    </div>
-    <div className="col s12 l9">
-      <div className="card z-depth-2 hoverable">
-        <div className="card-content">
-          <span className="card-title">123 Fake Street Springfield </span>
-          <p>Dates Occupied: 1/1/2000- 1/1/2020</p>
-          <p>Stars: </p>
-          <p>Review: Real shithole</p>
-          <button className="btn-large waves-effect waves-light">Edit Review</button>
-          <button className="btn-large waves-effect waves-light">Delete Review</button>
+      <div className="col s12 l9">
+        <div className="card z-depth-2 hoverable">
+          <div className="card-content">
+            <span className="card-title">123 Fake Street Springfield </span>
+            <p>Dates Occupied: 1/1/2000- 1/1/2020</p>
+            <p>Stars: </p>
+            <p>Review: Real shithole</p>
+            <button className="btn-large waves-effect waves-light">Edit Review</button>
+            <button className="btn-large waves-effect waves-light">Delete Review</button>
+          </div>
         </div>
       </div>
+      </div>
     </div>
-  </div>
-  </div>
   </div>
   )
 
