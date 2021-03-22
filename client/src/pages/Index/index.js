563ba7a9c78da3cf5f1863c4b9e8410207fec407
Dillon H <dhobs21@gmail.com>
2021-03-21 19:24:11 -0700

    updating landing

--- client/src/pages/Index/index.js
+++ client/src/pages/Index/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import ReadReviewsImg from "../../images/read-review-card-img.png";
 import WriteReviewImg from "../../images/write-review-card-img.png";
 
@@ -7,14 +8,21 @@ function Index() {
     <React.Fragment>
     <div className= "hero-image">
       <div className="hero-text">
-        <h2>Welcome Renter!</h2>
+        <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 4 }}>Welcome Renter!</motion.h1>
+        <motion.h5
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 4 }}>Here to keep you from getting stung by another bad living situation.</motion.h5>
       </div>
     </div>
     <main className="container">
       <div className="row">
         <div className="col s12 m2 l2"></div>
         <div className="col s12 m10 l8">
-          <p>We are here to spill all the tea on your next potential apartment or home. Our reviews could you avoid a disaster or find your next place to live. Reading our reviews is always free and easy; simply search for a unit by address. If you would like to create a review you will need to log in to your account or sign up. Happy hunting!</p>
+          <p>Our reviews could you avoid a disaster or find your next place to live. Reading our reviews is always free and easy; simply search for a unit by address. If you would like to create a review you will need to log in to your account or sign up. Happy hunting!</p>
         </div>
         <div className="col s12 m2 l2"></div>
       </div>
@@ -28,7 +36,7 @@ function Index() {
               </div>
               <span className="card-title">Read reviews</span>
               <div className="card-content">
-                <p>Search for reviews of the palaces you are interested in.</p>
+                <p>Search for reviews of the palaces you are looking at.</p>
               </div>
               <div className="card-action">
               <a href="/search-apt" className="waves-effect waves-light btn-large">Search</a>
@@ -42,7 +50,7 @@ function Index() {
               </div>
               <span className="card-title">Write a review</span>
               <div className="card-content">
-                <p>Help a future renter by leaving a review of the places you've lived.</p>
+                <p>Help a future renter by reviewing places you've lived.</p>
               </div>
               <div className="card-action">
               <a href="/write-review" className="waves-effect waves-light btn-large">New Review</a>
